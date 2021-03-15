383b476e4b8747a7d27c75212f3092fb2a5ac295
Ricko Caesar Aprilla Tiaka <bussiness.needs.rctka@gmail.com>
2021-03-16 01:13:08 +0800

    First Commit

---
+++ myapp/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <vs-navbar fixed :color="active" center-collapsed v-model="active">
+      <template #left>
+        Logo | TechDesignProgrammer
+      </template>
+      <template #default>
+        <vs-navbar-item :active="active == 'home'" id="home">
+          Home
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'news'" id="news">
+          News
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'blogs'" id="blogs">
+          Blogs
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'about'" id="about">
+          About Us
+        </vs-navbar-item>
+      </template>
+      <template #right>
+        <vs-navbar-group>
+          <i class="fi-ewsdxl-equilateral-triangle" style="font-size:8px;margin-left:80px;"></i>
+          <template #items>
+            <vs-navbar-item id="account">
+              <i class="fi-xnsuxl-user-solid" style="margin-right:5px;"></i>Account
+            </vs-navbar-item>
+            <vs-navbar-item id="settings">
+              <i class="fi-xnsuxl-setting-solid" style="margin-right:5px;"></i>Settings
+            </vs-navbar-item>
+            <vs-navbar-item id="logout">
+              <i class="fi-xnllxl-sign-out" style="margin-right:5px;"></i>Logout
+            </vs-navbar-item>
+          </template>
+        </vs-navbar-group>
+        <vs-avatar circle size="40" badge badge-color="success" style="margin-top:10px;margin-bottom:10px;">
+          <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" alt="">
+        </vs-avatar>
+        
+      </template>
+    </vs-navbar>
+    <div class="square">
+        <router-view></router-view>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data:() => ({
+      active: 'guide',
+      activeSidebar: false
+    })
+  }
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ myapp/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home" style="margin-top:3%;">
+    <agile ref="carousel" autoplay :autoplay-speed="8000" :timing="'ease'" :speed="1000">
+      <img class="slide" src="https://images.unsplash.com/photo-1506260408121-e353d10b87c7?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ" alt="">
+      <img class="slide" src="https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ" alt="">
+      <img class="slide" src="https://images.unsplash.com/photo-1524260855046-f743b3cdad07?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ" alt="">
+    </agile>
+    <div style="position:relative; margin-top:-20px;">
+      <button @click="$refs.carousel.goTo(1)" style="background-color:white;height:1vh;width:1vw;border-radius:50%;border:none;margin-left:5px;">
+      </button>
+      <button @click="$refs.carousel.goTo(2)" style="background-color:white;height:1vh;width:1vw;border-radius:50%;border:none;margin-left:5px;">
+      </button>
+      <button @click="$refs.carousel.goTo(3)" style="background-color:white;height:1vh;width:1vw;border-radius:50%;border:none;margin-left:5px;">
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {VueAgile} from 'vue-agile'
+
+export default {
+  name: 'Home',
+  components: {
+    agile: VueAgile,
+  }
+}
+</script>
+
+<style>
+.slide {
+  display: block;
+	height: 450px;
+	object-fit: cover;
+	width: 100%;
+}
+.agile__actions {
+  display: none;
+}
+
+</style>
